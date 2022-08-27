dedee3589e6bfdf3166ab1697cd8eb2014c06e59
Mike Preslar <80786299+MikePreslar@users.noreply.github.com>
2022-08-27 16:31:58 -0700

    update

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,10 @@
+import random
+  
 def main():
-  print('You rolled a die')
+dice_rolls = 2
+for i in range(0,dice_rolls):
+  roll = random.randint(1,6)
+  print(f'You rolled a {roll}')  
 
 if __name__== "__main__":
   main()
